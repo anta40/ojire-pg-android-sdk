70809dc456e2cd1f2cfa467ad9104f429d370b55
Andre Tampubolon <andre@lc.vlsm.org>
2026-01-28 20:58:44 +0700

    Added onClose

--- opgsdk/src/main/java/com/ojire/sdk/opg/OPGWebView.java
+++ opgsdk/src/main/java/com/ojire/sdk/opg/OPGWebView.java
@@ -1,9 +1,11 @@
 package com.ojire.sdk.opg;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.util.AttributeSet;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -18,6 +20,12 @@ public class OPGWebView extends WebView  {
     private OPGListener listener;
     private String currentUrl;
     private boolean urlIsChanged;
+    private Context ctxt;
+
+    public OPGWebView(Context ctxt){
+        super(ctxt);
+        this.ctxt = ctxt;
+    }
 
     public void setFoo(String newFoo){
         this.foo = newFoo;
@@ -31,33 +39,26 @@ public class OPGWebView extends WebView  {
         this.listener = listener;
     }
 
+    @JavascriptInterface
+    public void closeWebView() {
+        ((Activity)ctxt).finish();
+        if (listener != null){
+            listener.onClose();
+        }
+    }
+
     public OPGWebView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
-    }
-
-    private void init(Context context, AttributeSet attrs) {
-        // Retrieve the attributes defined in attrs.xml
-        TypedArray a = context.getTheme().obtainStyledAttributes(
-                attrs,
-                R.styleable.OPGWebView,
-                0, 0);
-
-        try {
-            foo = a.getString(R.styleable.OPGWebView_foo);
-        } finally {
-            // Always recycle the TypedArray to avoid memory leaks
-            a.recycle();
-        }
 
         setWebViewClient(new WebViewClient() {@Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                urlIsChanged = true;
-                currentUrl = url;
-                if (listener != null) {
-                    listener.onPending(url);
-                }
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            urlIsChanged = true;
+            currentUrl = url;
+            if (listener != null) {
+                listener.onPending(url);
             }
+        }
 
             @Override
             public void onPageFinished(WebView view, String url) {
@@ -81,5 +82,20 @@ public class OPGWebView extends WebView  {
                 }
             }
         });
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        // Retrieve the attributes defined in attrs.xml
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.OPGWebView,
+                0, 0);
+
+        try {
+            foo = a.getString(R.styleable.OPGWebView_foo);
+        } finally {
+            // Always recycle the TypedArray to avoid memory leaks
+            a.recycle();
+        }
     }
 }
