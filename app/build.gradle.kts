f44d5547c997c9ec8e89394764132476cf108d9a
Andre Tampubolon <andre@lc.vlsm.org>
2026-01-30 00:02:48 +0700

    Fix build.gradle.kts again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(project(":opgsdk"))
+    api(project(":opgsdk"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
