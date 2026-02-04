44633e3a2cccc125cc1ae0a624172d4a17b60e17
Andre Tampubolon <andre@lc.vlsm.org>
2026-02-04 20:31:39 +0700

    Unused codes are removed

--- opgsdk/src/main/java/com/ojire/sdk/opg/OPGDummyListener.java
+++
@@ -1,6 +0,0 @@
-package com.ojire.sdk.opg;
-
-public interface OPGDummyListener {
-    //void onUrlChanged(String oldUrl, String newUrl);
-    void onSuccess(String newUrl);
-}
--- opgsdk/src/main/java/com/ojire/sdk/opg/User.java
+++
@@ -1,9 +0,0 @@
-package com.ojire.sdk.opg;
-
-public class User {
-    private int id;
-    private String name;
-
-    // Getters and Setters
-    public String getName() { return name; }
-}
